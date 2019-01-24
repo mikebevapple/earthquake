bc1024470226c15d18c69f11f7541e80569b406d
Mike Beverley <mikeb@interknowlogy.com>
2019-01-23 17:46:59 -0800

    Corrected location to be driven by locationSource (not lat/long)

--- earthquake.py
+++ earthquake.py
@@ -5,19 +5,25 @@ def readCsvIntoDictionary():
     numberOfEarthquakesByLocation = {}
     highestEarthquakeCount = 0
     locationWithMostEarthquakes = ()
+    multipleLocationsTiedForMostEarthquakes = False
     with open('./data/1.0_month.csv') as earthquakeData:
         csvReader = csv.DictReader(earthquakeData)
         for row in csvReader:
-            location = (row["latitude"], row["longitude"])
+            locationSource = row["locationSource"]
             previousEarthquakeCountForLocation = 0
-            if location in numberOfEarthquakesByLocation :
-                previousEarthquakeCountForLocation = numberOfEarthquakesByLocation.get(location)
+            if locationSource in numberOfEarthquakesByLocation :
+                previousEarthquakeCountForLocation = numberOfEarthquakesByLocation.get(locationSource)
             newEarthquakeCountForLocation = previousEarthquakeCountForLocation + 1
-            numberOfEarthquakesByLocation[location] = previousEarthquakeCountForLocation
+            numberOfEarthquakesByLocation[locationSource] = newEarthquakeCountForLocation
             if highestEarthquakeCount < newEarthquakeCountForLocation :
                 highestEarthquakeCount = newEarthquakeCountForLocation
-                locationWithMostEarthquakes = location
-        print('Location with most earthquakes = {},{} ({})'.format(locationWithMostEarthquakes[0],locationWithMostEarthquakes[1],highestEarthquakeCount))
+                locationWithMostEarthquakes = locationSource
+                multipleLocationsTiedForMostEarthquakes = False
+            elif highestEarthquakeCount == newEarthquakeCountForLocation :
+                multipleLocationsTiedForMostEarthquakes = True
 
+        print('Location with most earthquakes ({}) = {}'.format(highestEarthquakeCount,locationWithMostEarthquakes))
+        if multipleLocationsTiedForMostEarthquakes :
+            print("*More than one location tied for most earthquakes")
 
 readCsvIntoDictionary()
